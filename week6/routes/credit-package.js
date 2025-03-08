019f0fde6affcd926b2b29cb08b6ca0480d28926
weikai <pythonforeye@gmail.com>
2025-03-09 01:55:50 +0800

    Attempt to use express-validator

--- week6/routes/credit-package.js
+++ week6/routes/credit-package.js
@@ -1,16 +1,12 @@
 const express = require('express')
+const { body, param, validationResult } = require('express-validator');
 
-const router = express.Router()
 const { dataSource } = require('../db/data-source')
-const logger = require('../utils/logger')('CreditPackage')
 
-const {
-	isNotValidString,
-	isNotValidInteger,
-	isNotValidUUID,
-} = require('../utils/validators');
+const logger = require('../utils/logger')('CreditPackage');
 const appError = require('../utils/app-error');
 
+const router = express.Router();
 const CreditPackageRepo = dataSource.getRepository('CreditPackage')
 
 router.get('/', async (req, res, next) => {
@@ -29,49 +25,59 @@ router.get('/', async (req, res, next) => {
 	}
 })
 
-router.post('/', async (req, res, next) => {
-	try {
-		const { name, credit_amount, price } = req.body;
-
-		if (isNotValidString(name) || isNotValidInteger(credit_amount) || isNotValidInteger(price)) {
+router.post('/',
+	body('name').isString().trim().notEmpty(),
+	body('credit_amount').notEmpty().isInt({ gt: 0 }),
+	body('price').notEmpty().isInt({ gt: 0 }),
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
 			return next(appError(400, '欄位未填寫正確'));
 		}
 
-		const isCreditPackageExist = await CreditPackageRepo.findOneBy({ name });
+		try {
+			const { name, credit_amount, price } = req.body;
 
-		if (isCreditPackageExist) {
-			return next(appError(409, '資料重複'));
+			const isCreditPackageExist = await CreditPackageRepo.findOneBy({ name });
+
+			if (isCreditPackageExist) {
+				return next(appError(409, '資料重複'));
+			}
+
+			const newCreditPackage = CreditPackageRepo.create({
+				name,
+				credit_amount,
+				price,
+			});
+			const result = await CreditPackageRepo.save(newCreditPackage);
+
+			res.status(200).json({
+				status: 'success',
+				data: {
+					id: result.id,
+					name: result.name,
+					credit_amount: result.credit_amount,
+					price: result.price,
+				},
+			});
+		} catch (error) {
+			logger.error(error);
+			next(error);
 		}
-
-		const newCreditPackage = CreditPackageRepo.create({
-			name,
-			credit_amount,
-			price,
-		});
-		const result = await CreditPackageRepo.save(newCreditPackage);
-
-		res.status(200).json({
-			status: 'success',
-			data: {
-				id: result.id,
-				name: result.name,
-				credit_amount: result.credit_amount,
-				price: result.price,
-			},
-		});
-	} catch (error) {
-		logger.error(error);
-		next(error);
 	}
-})
+);
 
-router.delete('/:creditPackageId', async (req, res, next) => {
+
+router.delete('/:creditPackageId', [
+	param('creditPackageId').isUUID().withMessage('ID錯誤')
+], async (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return next(appError(400, errors.array()[0].msg));
+	}
+
 	try {
 		const { creditPackageId } = req.params;
-
-		if (isNotValidUUID(creditPackageId)) {
-			return next(appError(400, 'ID錯誤'));
-		}
 
 		const result = await CreditPackageRepo.delete(creditPackageId);
 
